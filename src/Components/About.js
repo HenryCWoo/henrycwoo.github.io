3148d03552fc1c28701d2d22faa72fe0466c9a2a
Henry Woo <hcwoo2@illinois.edu>
2021-07-22 23:48:14 -0500

    Removed resume data because of privacy concerns

--- src/Components/About.js
+++ src/Components/About.js
@@ -62,14 +62,14 @@ class About extends Component {
                                     <span>{phone}</span>
                                 </p>
                             </div>
-                            <div className="columns download">
+                            {/* <div className="columns download">
                                 <p>
                                     <a href={resumeDownload} className="button">
                                         <i className="fa fa-download"></i>
                                         Resume
                                     </a>
                                 </p>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                 </div>
